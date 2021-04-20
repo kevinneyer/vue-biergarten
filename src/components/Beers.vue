a20a283276c17cb6a41ecdf925f02ed938af5065
Kevin Neyer <59579209+kevinneyer@users.noreply.github.com>
2021-04-20 11:31:34 -0400

    changed to biergarten

---
+++ src/components/Beers.vue
@@ -0,0 +1,35 @@
+<template>
+    <div>
+        All Beers
+        <div v-if="loaded">
+            <div v-for="beer in beers" :key="beer.id">
+                {{ beer.name }}
+            </div>
+        </div>
+    </div>
+</template>
+
+<script>
+export default {
+    name: 'Beers',
+    data(){
+        return{
+            beers: [],
+            loaded: false
+        }
+    },
+    methods: {
+        getBeers(){
+            const axios = require('axios');
+            axios.get('http://localhost:3001/api/v1/beers/')
+            .then((res) => {
+                this.beers = res.data;
+                this.loaded = true
+            })
+        }
+    },
+    mounted() {
+        this.getBeers()
+    }
+}
+</script>
---
+++ src/components/Home.vue
@@ -0,0 +1,17 @@
+<template>
+    <div>
+        Vue Biergarten
+        <Beers />
+    </div>
+</template>
+
+<script>
+import Beers from './Beers';
+
+export default {
+   name: 'Home',
+   components: {
+       Beers
+   }
+}
+</script>
